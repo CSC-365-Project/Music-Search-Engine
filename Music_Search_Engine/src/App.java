72bf7746da1fb403f9a044f048b98aa5c69a3234
GelzoneXUnsas <51806064+GelzoneXUnsas@users.noreply.github.com>
2022-05-03 13:45:38 -0700

    refactor App.java

--- Music_Search_Engine/src/App.java
+++ Music_Search_Engine/src/App.java
@@ -5,20 +5,34 @@ public class App {
     static Connection connect;
 
     public static void main(String[] args) throws Exception {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            connect = DriverManager.getConnection("jdbc:mysql://ambari-node5.csc.calpoly.edu:3306/musicsearchengine?user=musicsearchengine&password=88888888");
 
+            // Getting connection from shared db.
+            Connection connect = connectdb("musicsearchengine", "musicsearchengine", "88888888");
+
+            // sample query
             String selectAllUsers = "select * from TestUsers;";
             Statement statement = connect.createStatement();
             ResultSet rs = statement.executeQuery(selectAllUsers);
+
+            //sample printing
             while (rs.next()){
                 String userName= rs.getString(2);
                 System.out.println("Test User Name: " + userName);
             }
+
+    }
+
+    public static Connection connectdb(String hostname, String userName, String password){
+        // try to connect to db with given hostname, username, and password
+        // username and password might need further encode
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connect = DriverManager.getConnection("jdbc:mysql://ambari-node5.csc.calpoly.edu:3306/" + hostname + "?" + "user=" + userName + "&password=" + password);
+                 
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        return connect;  
     }
 }
