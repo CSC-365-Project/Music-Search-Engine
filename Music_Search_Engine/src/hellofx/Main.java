27b7b6ecfe2716bf879feea4069444591cff2efb
GelzoneXUnsas <51806064+GelzoneXUnsas@users.noreply.github.com>
2022-05-07 11:13:04 -0700

    update title

--- Music_Search_Engine/src/hellofx/Main.java
+++ Music_Search_Engine/src/hellofx/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
-        primaryStage.setTitle("music Search Engine");
+        primaryStage.setTitle("Music Search Engine");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
