6478f49b59a1bce6b0aeb275c36b9bd98d8c0d59
Kailin Ke <kaifawn127@gmail.com>
2022-05-19 12:56:53 -0700

    added alert, homepage display user email, and change scene after sign up

--- Music_Search_Engine/src/hellofx/LoginUtil.java
+++ Music_Search_Engine/src/hellofx/LoginUtil.java
@@ -6,8 +6,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.scene.control.Alert.AlertType;
 
 public class LoginUtil {
     public static void changeScene(ActionEvent event, String fxmlFile, String title, TextField email,
@@ -20,8 +22,14 @@ public class LoginUtil {
             if (Query.getPassword(email.getText()).compareTo(password.getText()) == 0) {
                 FXMLLoader loader = new FXMLLoader(LoginUtil.class.getResource(fxmlFile));
                 root = loader.load();
+                HomeController controller = loader.getController();
+                controller.initData(email);
             } else {
-                System.out.println("Password incorrect");
+                Alert a = new Alert(AlertType.WARNING);
+                a.setTitle("Alert");
+                a.setContentText("Password incorrect or user does not exist");
+                a.show();
+                return;
             }
         } else {
             root = FXMLLoader.load(LoginUtil.class.getResource(fxmlFile));
@@ -33,16 +41,37 @@ public class LoginUtil {
     }
 
     public static void signUpUser(ActionEvent event, TextField email, TextField username, TextField password,
-            TextField password_conf) {
+            TextField password_conf, String fxmlFile) throws IOException {
         Query.init();
+        Parent root = null;
         if (!email.getText().trim().isEmpty() && !username.getText().trim().isEmpty()
                 && !password.getText().trim().isEmpty()
                 && password.getText().compareTo(password_conf.getText()) == 0) {
-            Query.addNewUser(email.getText(), username.getText(), password.getText());
+            if (Query.getPassword(email.getText()).compareTo("") == 0) {
+                Query.init();
+                Query.addNewUser(email.getText(), username.getText(), password.getText());
+                FXMLLoader loader = new FXMLLoader(LoginUtil.class.getResource(fxmlFile));
+                root = loader.load();
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                stage.setTitle("Login");
+                stage.setScene(new Scene(root, 600, 400));
+                stage.show();
+            } else {
+                Alert a = new Alert(AlertType.WARNING);
+                a.setTitle("Alert");
+                a.setContentText("Account already exist");
+                a.show();
+            }
         } else if (password.getText().compareTo(password_conf.getText()) != 0) {
-            System.out.println("Password does not match");
+            Alert a = new Alert(AlertType.WARNING);
+            a.setTitle("Alert");
+            a.setContentText("Password does not match");
+            a.show();
         } else {
-            System.out.println("one or more of email, username, or password is blank");
+            Alert a = new Alert(AlertType.WARNING);
+            a.setTitle("Alert");
+            a.setContentText("one or more of email, username, or password is blank");
+            a.show();
         }
 
     }
